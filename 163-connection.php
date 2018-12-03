3cf80fc7f47c4c7340e53944ee7673537fa2e781
Jintae Lee <37683734+TAEKnical@users.noreply.github.com>
2018-12-03 22:58:15 +0900

    Add files via upload

---
+++ 163-connection.php
@@ -0,0 +1,15 @@
+<?php
+  $host="localhost";
+  $user="root";
+  $pw="root";
+  $dbName="php200project";
+  $dbConnect = new mysqli($host,$user,$pw,$dbName);
+  $dbConnect->set_charset("utf8");
+  if(mysqli_connect_errno()){
+    echo "데이터베이스 접속 실패";
+  }
+  else {
+    echo "접속 성공";
+    echo "<br>";
+  }
+ ?>
---
+++ 165-index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="167-layoutCSS.php" />
+  </head>
+  <body>
+    <div id="wrap">
+      <div id="header"></div>
+      <section id="leftArea"></section>
+      <aside id="rightArea"></aside>
+      <div id="footer"></div>
+    </div>
+    </div>
+  </body>
+</html>
